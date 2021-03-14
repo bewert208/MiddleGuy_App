ac861ef789473685109093f3de9307238bfc6477
Ben Ewert <baewer01@louisville.edu>
2021-03-14 16:26:45 -0400

    Login screen first iteration

--- app/src/main/java/cecs550/middleguyapp/MainActivity.java
+++ app/src/main/java/cecs550/middleguyapp/MainActivity.java
@@ -2,13 +2,21 @@ package cecs550.middleguyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
