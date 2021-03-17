5768c98b0df0496c8ed87b0406f620b5adddb97d
Ben Ewert <baewer01@louisville.edu>
2021-03-16 21:04:22 -0400

    Added to profile screen.

--- app/src/main/java/cecs550/middleguyapp/ThirdFragment.java
+++ app/src/main/java/cecs550/middleguyapp/ThirdFragment.java
@@ -1,16 +1,26 @@
 package cecs550.middleguyapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ThirdFragment extends Fragment {
 
     public ThirdFragment(){
     }
+
 
 
 
@@ -22,6 +32,8 @@ public class ThirdFragment extends Fragment {
                 container, false);
 
     }
+
+
 
 
 }
